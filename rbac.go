3927dbdb26cea064d4b5eba0b56bbbe3a9f233c7
Angad Nadkarni <angad.nadkarni@gmail.com>
2021-01-12 23:12:59 +0530

    Added ClientId

--- rbac.go
+++ rbac.go
@@ -3,10 +3,14 @@ package auth
 import (
 	"context"
 
+	"github.com/angadn/config"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/cognitoidentityprovider"
 )
+
+// AWSCognitoClientID is the name of an environment variable we use to configure Cognito.
+const AWSCognitoClientID = "AWS_COGNITO_CLIENT_ID"
 
 // RBAC is an interface that any RBAC provider must implement.
 type RBAC interface {
@@ -18,12 +22,17 @@ type RBAC interface {
 // AWSCognitoRBAC implements the RBAC interface for AWS Cognito.
 type AWSCognitoRBAC struct {
 	ses *session.Session
+	cfg config.Source
 }
 
 // NewAWSCognitoRBAC is the provider for an AWS Cognito-backed Repository.
-func NewAWSCognitoRBAC(awsSession *session.Session) (rbac RBAC, err error) {
+func NewAWSCognitoRBAC(
+	cfg config.Source,
+	awsSession *session.Session,
+) (rbac RBAC, err error) {
 	ret := new(AWSCognitoRBAC)
 	ret.ses = awsSession
+	ret.cfg = cfg
 	rbac = ret
 	return
 }
@@ -32,8 +41,14 @@ func NewAWSCognitoRBAC(awsSession *session.Session) (rbac RBAC, err error) {
 func (repo *AWSCognitoRBAC) Authenticate(
 	ctx context.Context, username, password string,
 ) (user User, ok bool, err error) {
+	var clientID config.Value
+	if clientID, err = repo.cfg.Get(ctx, config.Key(AWSCognitoClientID)); err != nil {
+		return
+	}
+
 	var cip = cognitoidentityprovider.New(repo.ses)
 	if _, err = cip.InitiateAuth(&cognitoidentityprovider.InitiateAuthInput{
+		ClientId: aws.String(string(clientID)),
 		AuthFlow: aws.String("USER_PASSWORD_AUTH"),
 		AuthParameters: map[string]*string{
 			"USERNAME": aws.String(username),
